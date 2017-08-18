d09fd5e13fbbc24a1b43505f181a6c5c0d8f3c85
wuqiangroy <wuqiangroy@live.com>
2017-08-18 18:03:48 +0800

    point

---
+++ point.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i, j := 42, 2701
+	//&符号会生成一个指向其作用对象的指针
+	p := &i
+	fmt.Print(p, "\n")
+	*p = 21
+	fmt.Print(i, "\n")
+	
+	p = &j
+	*p = *p / 37
+	fmt.Print(j)
+}
