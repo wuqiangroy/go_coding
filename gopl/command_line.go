b6fd2ca08c63f906e3924611f6008b439fcf4700
wuqiangroy <wuqiangroy@live.com>
2017-09-28 23:57:58 +0800

    add another way to explain

--- gopl/command_line.go
+++ gopl/command_line.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 //how to run this program
 //as python, go run command_line.go "first" "second"
@@ -10,9 +11,18 @@ import (
 
 func main() {
 	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = "  "
+	// for i := 1; i < len(os.Args); i++ {
+	// 	s += sep + os.Args[i]
+	// 	sep = "  "
+	// }
+
+	//  _ is index and args is value : os.Args[index]
+	//  _ means blank indentifier
+	for _, args := range os.Args[1:] {
+		s += sep + args
+		sep = "   "
 	}
 	fmt.Println(s)
+	// another way to print os.Args
+	fmt.Println(strings.Join(os.Args[1:], "   "))
 }
