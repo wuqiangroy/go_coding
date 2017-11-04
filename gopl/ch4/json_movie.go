3a5053baaf090071786c5af371db02717340b052
wuqiangroy <wuqiangroy@live.com>
2017-11-04 13:20:10 +0800

    json go

---
+++ gopl/ch4/json_movie.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	"log"
+)
+
+type Movie struct {
+	Title string
+	Year int
+	Color bool
+	Actors []string
+}
+
+func main() {
+	var movie = []Movie{
+		{Title: "Casablanca", Year: 1942, Color: false,
+			Actors: []string{"Humphrey Bogart", "Ingrid Bergman"}},
+		{Title: "Cool Hand Luke", Year: 1967, Color: true,
+			Actors: []string{"Paul Newman"}},
+		{Title: "Bullitt", Year: 1968, Color: true,
+			Actors:[]string{"steve MacQueen", "Jacqueline Bisset"}},
+	}
+	data, err := json.Marshal(movie)
+	if err != nil {
+		log.Fatalf("JSON marshling failed: %s", err)
+	}
+	fmt.Printf("%s\n", data)
+
+	// formating print
+	data2, err := json.MarshalIndent(movie, "", "    ")
+	if err != nil {
+		log.Fatalf("JSON marshaling failed: %s", err)
+	}
+	fmt.Printf("%s\n", data2)
+}
